bbccb6fcd8969e721882672ff5faee889d6f9e91
ralucaMi <39234203+ralucaMi@users.noreply.github.com>
2018-11-17 11:44:20 +0200

    thing

---
+++ Tema2.cpp
@@ -0,0 +1,120 @@
+#include "Tema2.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <Core/Engine.h>
+#include "Transform3D.h"
+
+using namespace std;
+
+Tema2::Tema2()
+{
+}
+
+Tema2::~Tema2()
+{
+}
+
+void Tema2::Init()
+{
+	polygonMode = GL_FILL;
+
+	Mesh* mesh = new Mesh("box");
+	mesh->LoadMesh(RESOURCE_PATH::MODELS + "Primitives", "box.obj");
+	meshes[mesh->GetMeshID()] = mesh;
+
+	// initialize tx, ty and tz (the translation steps)
+	translateX = 0;
+	translateY = 0;
+	translateZ = 0;
+
+	// initialize sx, sy and sz (the scale factors)
+	scaleX = 1;
+	scaleY = 1;
+	scaleZ = 1;
+
+	// initialize angularSteps
+	angularStepOX = 0;
+	angularStepOY = 0;
+	angularStepOZ = 0;
+}
+
+void Tema2::FrameStart()
+{
+	// clears the color buffer (using the previously set color) and depth buffer
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glm::ivec2 resolution = window->GetResolution();
+	// sets the screen area where to draw
+	glViewport(0, 0, resolution.x, resolution.y);
+}
+
+void Tema2::Update(float deltaTimeSeconds)
+{
+	glLineWidth(3);
+	glPointSize(5);
+	glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
+
+	
+}
+
+void Tema2::FrameEnd()
+{
+	DrawCoordinatSystem();
+}
+
+void Tema2::OnInputUpdate(float deltaTime, int mods)
+{
+	// TODO
+}
+
+void Tema2::OnKeyPress(int key, int mods)
+{
+	// add key press event
+	if (key == GLFW_KEY_SPACE)
+	{
+		switch (polygonMode)
+		{
+		case GL_POINT:
+			polygonMode = GL_FILL;
+			break;
+		case GL_LINE:
+			polygonMode = GL_POINT;
+			break;
+		default:
+			polygonMode = GL_LINE;
+			break;
+		}
+	}
+}
+
+void Tema2::OnKeyRelease(int key, int mods)
+{
+	// add key release event
+}
+
+void Tema2::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
+{
+	// add mouse move event
+}
+
+void Tema2::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
+{
+	// add mouse button press event
+}
+
+void Tema2::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
+{
+	// add mouse button release event
+}
+
+void Tema2::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
+{
+}
+
+void Tema2::OnWindowResize(int width, int height)
+{
+}
